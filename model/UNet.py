aa69a62e85fc75122dd2d9b72f2820c5e2cf61f4
CKWaFer <49187580+CKWaFer@users.noreply.github.com>
2024-09-26 10:40:18 +0800

    Create UNet.py

---
+++ model/UNet.py
@@ -0,0 +1,64 @@
+import torch
+import torch.nn as nn
+
+
+class UNet(nn.Module):
+    def __init__(self, in_channels=1, out_channels=1):
+        super(UNet, self).__init__()
+        
+        def conv_block(in_channels, out_channels):
+            return nn.Sequential(
+                nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1),
+                nn.BatchNorm2d(out_channels),
+                nn.ReLU(inplace=True),
+                nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1),
+                nn.BatchNorm2d(out_channels),
+                nn.ReLU(inplace=True)
+            )
+        
+        def upconv_block(in_channels, out_channels):
+            return nn.Sequential(
+                nn.ConvTranspose2d(in_channels, out_channels, kernel_size=2, stride=2),
+                nn.BatchNorm2d(out_channels),
+                nn.ReLU(inplace=True)
+            )
+        
+        self.enc1 = conv_block(in_channels, 64)
+        self.enc2 = conv_block(64, 128)
+        self.enc3 = conv_block(128, 256)
+        self.enc4 = conv_block(256, 512)
+        
+        self.pool = nn.MaxPool2d(2)
+        
+        self.upconv3 = upconv_block(512, 256)
+        self.upconv2 = upconv_block(256, 128)
+        self.upconv1 = upconv_block(128, 64)
+        
+        self.decoder3 = conv_block(512, 256)
+        self.decoder2 = conv_block(256, 128)
+        self.decoder1 = conv_block(128, 64)
+        
+        self.final = nn.Conv2d(64, out_channels, kernel_size=1)
+
+    def forward(self, x):
+        # Encoding
+        enc1 = self.enc1(x)
+        enc2 = self.enc2(self.pool(enc1))
+        enc3 = self.enc3(self.pool(enc2))
+        enc4 = self.enc4(self.pool(enc3))
+        
+        # Decoding
+        dec3 = self.upconv3(enc4)
+        dec3 = torch.cat((dec3, enc3), dim=1)
+        dec3 = self.decoder3(dec3)
+        
+        dec2 = self.upconv2(dec3)
+        dec2 = torch.cat((dec2, enc2), dim=1)
+        dec2 = self.decoder2(dec2)
+        
+        dec1 = self.upconv1(dec2)
+        dec1 = torch.cat((dec1, enc1), dim=1)
+        dec1 = self.decoder1(dec1)
+        
+        out = self.final(dec1)
+        return out
